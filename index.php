4ce9ecaf14912ce54ef5131b7fd8330d48c017b5
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-10-07 20:34:38 +0200

    Output images
    Add a shortcode function hat outputs images in a div;
    accepts image IDs, seperated by commas
    e.g. [x]253,242,418[/x]

--- index.php
+++ index.php
@@ -12,3 +12,15 @@ function plugin_enqueue_scripts() {
     wp_enqueue_style( 'plugin-styles', plugin_dir_url( __FILE__ ) . "/assets/css/style.css", '', '20191007');
 }
 add_action( 'wp_enqueue_scripts', 'plugin_enqueue_scripts' );
+
+// Accept image IDs, seperated by commas and output images in a flexbox div
+function plugin_display_logos($atts, $content = NULL) {
+    $array1 = preg_split("/[,]+/", $content );
+    $output = '<div>';
+    foreach ($array1 as $value) {
+        $image = wp_get_attachment_url($value);
+        $output .= '<img src="' . $image . '" />';
+    }
+    return $output . '</div>';
+}
+add_shortcode('x', 'plugin_display_logos');
